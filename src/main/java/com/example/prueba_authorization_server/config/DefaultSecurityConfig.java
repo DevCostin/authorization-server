987ecbad000d4c611a7d31c468dcd99cd849ffbe
devcostin <devcostin@gmail.com>
2022-12-13 13:19:24 +0100

    nuevo v2

--- src/main/java/com/example/prueba_authorization_server/config/DefaultSecurityConfig.java
+++ src/main/java/com/example/prueba_authorization_server/config/DefaultSecurityConfig.java
@@ -1,43 +1,36 @@
-package com.example.prueba_authorization_server;
+package com.example.prueba_authorization_server.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.Objects;
-
 import static org.springframework.security.config.Customizer.withDefaults;
-import static org.springframework.security.core.userdetails.User.builder;
-
 @EnableWebSecurity
+@Configuration(proxyBeanMethods = false)
 public class DefaultSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests(authorizeRequests ->
-                        authorizeRequests.anyRequest().authenticated()
+        http
+                .authorizeHttpRequests(authorize ->
+                        authorize.anyRequest().authenticated()
                 )
                 .formLogin(withDefaults());
         return http.build();
     }
-
     @Bean
     UserDetailsService users() {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        User.UserBuilder var10000 = builder();
-        Objects.requireNonNull(encoder);
-
-        UserDetails user = var10000.passwordEncoder(encoder::encode).username("admin")
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user1")
                 .password("password")
+                .roles("USER")
                 .build();
-
         return new InMemoryUserDetailsManager(user);
     }
 }
